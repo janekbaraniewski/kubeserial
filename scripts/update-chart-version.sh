20b74bab84905eb4880390a7ef98ee177960c446
Jan Baraniewski <dev@baraniewski.com>
2022-03-30 23:55:01 +0200

    Support both linux and mac and fix env subst in script

--- scripts/update-chart-version.sh
+++ scripts/update-chart-version.sh
@@ -4,5 +4,14 @@ CHART_PATH="./deploy/chart/kubeserial"
 PLACEHOLDER_VALUE="APP_VERSION"
 VERSION="works!"
 
-find ./deploy/chart/kubeserial \( -type d -name .git -prune \) -o -type f | \
-    xargs -0 sed -i 's/${PLACEHOLDER_VALUE}/${VERSION}/g'
+sed="sed"
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     sed=sed;;
+    Darwin*)    sed=gsed;;
+    *)          sed="UNKNOWN:${unameOut}"
+esac
+
+find ${CHART_PATH} \( -type d -name .git -prune \) -o -type f | \
+    xargs ${sed} -i 's/'"${PLACEHOLDER_VALUE}"'/'"${VERSION}"'/g'

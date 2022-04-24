07105081025a6d550dc090781c468a5a6f36d7c5
Jan Baraniewski <dev@baraniewski.com>
2022-04-24 23:44:06 +0200

    Update device crd spec

--- pkg/apis/kubeserial/v1alpha1/device.go
+++ pkg/apis/kubeserial/v1alpha1/device.go
@@ -20,16 +20,60 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+type DeviceConditionType string
+
+const (
+	DeviceAvailable DeviceConditionType = "Available"
+)
+
 // +k8s:openapi-gen=true
 // DeviceSpec defines the desired state of Device
 type DeviceSpec struct {
-	Foo string `json:"foo,omitempty"`
+	Name      string `json:"name"`
+	IdVendor  string `json:"idVendor"`
+	IdProduct string `json:"idProduct"`
+	Manager   string `json:"manager"`
+	Subsystem string `json:"subsystem"`
+}
+
+type DeviceCondition struct {
+	// +required
+	Type DeviceConditionType `json:"type" protobuf:"bytes,1,opt,name=type"`
+	// +required
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Enum=True;False;Unknown
+	Status metav1.ConditionStatus `json:"status" protobuf:"bytes,2,opt,name=status"`
+	// +optional
+	// +kubebuilder:validation:Minimum=0
+	ObservedGeneration int64 `json:"observedGeneration,omitempty" protobuf:"varint,3,opt,name=observedGeneration"`
+	// +required
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:Format=date-time
+	LastTransitionTime metav1.Time `json:"lastTransitionTime" protobuf:"bytes,4,opt,name=lastTransitionTime"`
+	// +optional
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:Format=date-time
+	LastHeartbeatTime metav1.Time `json:"lastHeartbeatTime" protobuf:"bytes,4,opt,name=lastHeartbeatTime"`
+	// +required
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:MaxLength=1024
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:Pattern=`^[A-Za-z]([A-Za-z0-9_,:]*[A-Za-z0-9_])?$`
+	Reason string `json:"reason" protobuf:"bytes,5,opt,name=reason"`
+	// message is a human readable message indicating details about the transition.
+	// This may be an empty string.
+	// +required
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:MaxLength=32768
+	Message string `json:"message" protobuf:"bytes,6,opt,name=message"`
 }
 
 // +k8s:openapi-gen=true
 // DeviceStatus defines the observed state of Device
 type DeviceStatus struct {
-	Available bool `json:"available"`
+	Conditions []DeviceCondition `json:"conditions"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object

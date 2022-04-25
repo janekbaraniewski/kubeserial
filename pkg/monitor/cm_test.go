131c01bd1be854208d86afc8d573392be9f81196
Jan Baraniewski <dev@baraniewski.com>
2022-04-25 16:23:57 +0200

    Add tests for monitor config

---
+++ pkg/monitor/cm_test.go
@@ -0,0 +1,29 @@
+package monitor
+
+import (
+	"testing"
+
+	appv1alpha1 "github.com/janekbaraniewski/kubeserial/pkg/apis/kubeserial/v1alpha1"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateConfigMap(t *testing.T) {
+	cr := &appv1alpha1.KubeSerial{
+		Spec: appv1alpha1.KubeSerialSpec{
+			Devices: []appv1alpha1.Device_2{
+				{
+					Name:      "testdevice",
+					Subsystem: "tty",
+					IdVendor:  "123",
+					IdProduct: "456",
+				},
+			},
+		},
+	}
+
+	result := CreateConfigMap(cr)
+
+	expectedUdevConfig := "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"123\", ATTRS{idProduct}==\"456\", SYMLINK+=\"testdevice\"\n"
+
+	assert.Equal(t, expectedUdevConfig, result.Data["98-devices.rules"])
+}

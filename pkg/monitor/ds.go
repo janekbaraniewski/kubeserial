10b483143201c8a65bc250ae32ceb44521a8c950
Jan Baraniewski <dev@baraniewski.com>
2022-04-26 02:52:26 +0200

    Fix run command for device monitor

--- pkg/monitor/ds.go
+++ pkg/monitor/ds.go
@@ -83,7 +83,7 @@ func CreateDaemonSet(cr *appv1alpha1.KubeSerial, monitorVersion string) *appsv1.
 							Command: []string{"/bin/sh"},
 							Args: []string{
 								"-c",
-								"./go/bin/device-monitor",
+								"./bin/device-monitor",
 							},
 							Env: []corev1.EnvVar{
 								{

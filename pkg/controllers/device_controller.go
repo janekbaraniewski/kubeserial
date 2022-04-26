f39ced828d9b3ad9dee9fd8acc9aa5600c11ddc1
Jan Baraniewski <dev@baraniewski.com>
2022-04-26 19:45:33 +0200

    Update device controller docstrings

--- pkg/controllers/device_controller.go
+++ pkg/controllers/device_controller.go
@@ -46,13 +46,6 @@ type DeviceReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Device object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
 func (r *DeviceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	devLog.Info("Starting device reconcile", "req", req)
 
@@ -120,6 +113,7 @@ func (r *DeviceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 	return ctrl.Result{}, nil
 }
 
+// ManagerIsAvailable checks if Manager object referenced by Device is available in the cluster
 func (r *DeviceReconciler) ManagerIsAvailable(ctx context.Context, device *kubeserialv1alpha1.Device, req ctrl.Request) bool {
 	log := devLog.WithName("ManagerIsAvailable")
 	manager := &kubeserialv1alpha1.Manager{}

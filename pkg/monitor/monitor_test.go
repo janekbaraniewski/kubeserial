7066a25c624c2b31b07cc5ba958ad63c2700b0dc
Jan Baraniewski <dev@baraniewski.com>
2022-04-27 21:22:09 +0200

    Test filttering out devices

--- pkg/monitor/monitor_test.go
+++ pkg/monitor/monitor_test.go
@@ -40,34 +40,38 @@ func TestUpdateDeviceState_ConfigMap(t *testing.T) {
 
 func TestUpdateDeviceState_Device(t *testing.T) {
 	ctx := context.Background()
-	device := &v1alpha1.Device{
-		ObjectMeta: v1.ObjectMeta{
-			Name:      "test-device",
-			Namespace: "test-ns",
-		},
-		Spec: v1alpha1.DeviceSpec{
-			Name:      "test-device",
-			IdVendor:  "123",
-			IdProduct: "456",
-			Subsystem: "tty",
-			Manager:   "test-manager",
-		},
-		Status: v1alpha1.DeviceStatus{
-			Conditions: []v1alpha1.DeviceCondition{
-				{
-					Type:   v1alpha1.DeviceReady,
-					Status: v1.ConditionTrue,
-				},
-				{
-					Type:   v1alpha1.DeviceAvailable,
-					Status: v1.ConditionUnknown,
+	getDevice := func(ready, available v1.ConditionStatus) *v1alpha1.Device {
+		return &v1alpha1.Device{
+			ObjectMeta: v1.ObjectMeta{
+				Name:      "test-device",
+				Namespace: "test-ns",
+			},
+			Spec: v1alpha1.DeviceSpec{
+				Name:      "test-device",
+				IdVendor:  "123",
+				IdProduct: "456",
+				Subsystem: "tty",
+				Manager:   "test-manager",
+			},
+			Status: v1alpha1.DeviceStatus{
+				Conditions: []v1alpha1.DeviceCondition{
+					{
+						Type:   v1alpha1.DeviceReady,
+						Status: ready,
+					},
+					{
+						Type:   v1alpha1.DeviceAvailable,
+						Status: available,
+						Reason: "testing",
+					},
 				},
 			},
-		},
+		}
 	}
 	os.Setenv("NODE_NAME", "test-node")
 	{
 		t.Run("test-update-available-condition-when-available", func(t *testing.T) {
+			device := getDevice(v1.ConditionTrue, v1.ConditionFalse)
 			fakeClientset := testclient.NewSimpleClientset()
 			fakeClientsetKubeserial := fake.NewSimpleClientset(device)
 			fs := afero.NewMemMapFs()
@@ -89,7 +93,7 @@ func TestUpdateDeviceState_Device(t *testing.T) {
 	}
 	{
 		t.Run("test-update-available-condition-when-unavailable", func(t *testing.T) {
-			device.Status.Conditions[1].Status = v1.ConditionTrue
+			device := getDevice(v1.ConditionTrue, v1.ConditionTrue)
 			device.Status.NodeName = "test-node"
 			fakeClientset := testclient.NewSimpleClientset()
 			fakeClientsetKubeserial := fake.NewSimpleClientset(device)
@@ -106,6 +110,24 @@ func TestUpdateDeviceState_Device(t *testing.T) {
 			assert.Equal(t, v1.ConditionFalse, availableCondition.Status)
 			assert.Equal(t, "DeviceUnavailable", availableCondition.Reason)
 			assert.Equal(t, "", foundDevice.Status.NodeName)
+		})
+	}
+	{
+		t.Run("test-dont-update-not-ready-device", func(t *testing.T) {
+			device := getDevice(v1.ConditionFalse, v1.ConditionUnknown)
+			fakeClientset := testclient.NewSimpleClientset()
+			fakeClientsetKubeserial := fake.NewSimpleClientset(device)
+			monitor := NewMonitor(fakeClientset, fakeClientsetKubeserial, "test-ns", os.Stat)
+
+			monitor.UpdateDeviceState(ctx)
+
+			foundDevice, err := fakeClientsetKubeserial.AppV1alpha1().Devices("test-ns").Get(
+				ctx, device.Name, v1.GetOptions{})
+
+			assert.Equal(t, nil, err)
+			availableCondition := utils.GetCondition(foundDevice.Status.Conditions, v1alpha1.DeviceAvailable)
+			assert.Equal(t, v1.ConditionUnknown, availableCondition.Status)
+			assert.Equal(t, "testing", availableCondition.Reason)
 		})
 	}
 	os.Unsetenv("NODE_NAME")

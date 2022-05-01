b892b7d5efbdd62ebf29313ebd2c05d448be9f18
Jan Baraniewski <dev@baraniewski.com>
2022-05-01 22:38:39 +0200

    Add initial webhook setup

--- hack/update-webhook-template.py
+++ hack/update-webhook-template.py
@@ -16,7 +16,8 @@ with open(crd_path) as f:
 with open(metadata_template_path) as f:
     metadata_template = yaml.safe_load(f)
 
-crd_yaml['metadata']['labels'] = metadata_template['labels']
+crd_yaml['metadata'] = metadata_template['metadata']
+crd_yaml['webhooks'][0]['clientConfig'] = metadata_template['clientConfig']
 
 with open(crd_path, 'w') as f:
-    yaml.dump(crd_yaml, f,  default_style=None, default_flow_style=False, width=4096)
+    yaml.dump(crd_yaml, f, default_style=None, default_flow_style=False, width=4096)

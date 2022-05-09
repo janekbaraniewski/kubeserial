670a78ad9ce4d2236e4dcf277efbda392458d0ea
Jan Baraniewski <dev@baraniewski.com>
2022-05-09 10:54:53 +0200

    Cleanup old gateway creation methods

--- pkg/gateway/deployment_test.go
+++ pkg/gateway/deployment_test.go
@@ -5,11 +5,19 @@ import (
 
 	"github.com/janekbaraniewski/kubeserial/pkg/apis/kubeserial/v1alpha1"
 	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestCreateDeployment(t *testing.T) {
 	// TODO: improve this test
-	deployment := CreateDeployment(&v1alpha1.KubeSerial{}, &v1alpha1.Device_2{}, "test-node")
+	deployment := CreateDeployment(&v1alpha1.Device{
+		ObjectMeta: v1.ObjectMeta{
+			Name: "test-device",
+		},
+		Status: v1alpha1.DeviceStatus{
+			NodeName: "test-node",
+		},
+	})
 
 	assert.Equal(t, map[string]string{
 		"kubernetes.io/hostname": "test-node",
--- pkg/gateway/svc.go
+++ pkg/gateway/svc.go
@@ -9,36 +9,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
-func CreateService(cr *appv1alpha1.KubeSerial, device *appv1alpha1.Device_2) *corev1.Service {
-	labels := map[string]string{
-		"app": cr.Name + "-" + device.Name + "-gateway",
-	}
-	name := strings.ToLower(cr.Name + "-" + device.Name + "-gateway")
-	return &corev1.Service{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: cr.Namespace,
-			Labels:    labels,
-		},
-		Spec: corev1.ServiceSpec{
-			Ports: []corev1.ServicePort{
-				{
-					Name:       "ser2net",
-					Protocol:   corev1.ProtocolTCP,
-					Port:       3333,
-					TargetPort: intstr.FromInt(3333),
-				},
-			},
-			Selector: map[string]string{
-				"app": name,
-			},
-			Type: corev1.ServiceTypeClusterIP,
-		},
-	}
-
-}
-
-func CreateServiceNew(device *appv1alpha1.Device) *corev1.Service {
+func CreateService(device *appv1alpha1.Device) *corev1.Service {
 	labels := map[string]string{
 		"app": device.Name + "-gateway",
 	}

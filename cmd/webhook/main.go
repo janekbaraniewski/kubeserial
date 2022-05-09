71f10522c795a8944550541eff16f324c448a3cf
Jan Baraniewski <dev@baraniewski.com>
2022-05-09 10:55:45 +0200

    Rename webhook

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -92,7 +92,7 @@ func main() {
 	hookServer.CertDir = params.certDir
 
 	entryLog.Info("registering webhooks to the webhook server")
-	hookServer.Register("/mutate-add-sidecar", &webhook.Admission{Handler: &webhooks.SidecarInjector{Name: "Logger", Client: mgr.GetClient(), Config: config}})
+	hookServer.Register("/mutate-add-sidecar", &webhook.Admission{Handler: &webhooks.SidecarInjector{Name: "DeviceSidecarInjector", Client: mgr.GetClient(), Config: config}})
 
 	entryLog.Info("starting manager")
 	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {

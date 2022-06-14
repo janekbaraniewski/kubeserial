17c2b31c990ca122b5cf575e44eca491ba46580a
Jan Baraniewski <dev@baraniewski.com>
2022-06-15 00:18:37 +0200

    Clear test-assets dir before generating new one

--- hack/get-test-assets.sh
+++ hack/get-test-assets.sh
@@ -2,6 +2,6 @@
 
 mkdir -p /tmp/kubeserial/test-assets
 helm template --name-template kubeserial --show-only templates/config_map.yaml charts/kubeserial > /tmp/kubeserial/test-assets/config-map.yaml
-mkdir test-assets
+rm -r test-assets && mkdir test-assets
 ls charts/kubeserial/specs | xargs -I{} bash -c "yq eval '.data[\"{}\"]' /tmp/kubeserial/test-assets/config-map.yaml > test-assets/{}"
 rm -r /tmp/kubeserial/test-assets

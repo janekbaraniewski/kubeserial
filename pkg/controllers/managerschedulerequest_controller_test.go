fb4920a8111f49dd5e9ec58259e9b383e2f1c67e
Jan Baraniewski <dev@baraniewski.com>
2022-06-15 05:10:01 +0200

    ManagerScheduleRequest controller coverage

--- pkg/controllers/managerschedulerequest_controller_test.go
+++ pkg/controllers/managerschedulerequest_controller_test.go
@@ -4,8 +4,13 @@ import (
 	"context"
 	"testing"
 
+	kubeserial "github.com/janekbaraniewski/kubeserial/pkg"
 	"github.com/janekbaraniewski/kubeserial/pkg/apis/v1alpha1"
+	"github.com/janekbaraniewski/kubeserial/pkg/kubeapi"
+	"github.com/janekbaraniewski/kubeserial/pkg/utils"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	controllerruntime "sigs.k8s.io/controller-runtime"
@@ -24,4 +29,100 @@ func TestManagerScheduleRequestReconcile(t *testing.T) {
 	}
 
 	reconciler.Reconcile(context.TODO(), controllerruntime.Request{})
+}
+
+func TestManagerScheduleRequestReconcile_DeviceFoundNoManager(t *testing.T) {
+	scheme := runtime.NewScheme()
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(v1alpha1.AddToScheme(scheme))
+
+	msr := &v1alpha1.ManagerScheduleRequest{
+		ObjectMeta: v1.ObjectMeta{
+			Name: "test-schedule-request",
+		},
+		Spec: v1alpha1.ManagerScheduleRequestSpec{
+			Device: "test-device",
+		},
+	}
+
+	device := &v1alpha1.SerialDevice{
+		ObjectMeta: v1.ObjectMeta{
+			Name: "test-device",
+		},
+	}
+
+	fakeClient := runtimefake.NewClientBuilder().WithScheme(scheme).Build()
+	fakeClient.Create(context.TODO(), msr)
+	fakeClient.Create(context.TODO(), device)
+
+	reconciler := ManagerScheduleRequestReconciler{
+		Client: fakeClient,
+		Scheme: scheme,
+	}
+
+	reconciler.Reconcile(context.TODO(), controllerruntime.Request{NamespacedName: types.NamespacedName{
+		Name: "test-schedule-request",
+	}})
+}
+
+func TestManagerScheduleRequestReconcile_DeviceManagerFound(t *testing.T) {
+	scheme := runtime.NewScheme()
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(v1alpha1.AddToScheme(scheme))
+
+	msr := &v1alpha1.ManagerScheduleRequest{
+		ObjectMeta: v1.ObjectMeta{
+			Name: "test-schedule-request",
+		},
+		Spec: v1alpha1.ManagerScheduleRequestSpec{
+			Device:  "test-device",
+			Manager: "test-manager",
+		},
+	}
+
+	device := &v1alpha1.SerialDevice{
+		ObjectMeta: v1.ObjectMeta{
+			Name: "test-device",
+		},
+	}
+
+	manager := &v1alpha1.Manager{
+		ObjectMeta: v1.ObjectMeta{
+			Name: "test-manager",
+		},
+	}
+
+	fakeClient := runtimefake.NewClientBuilder().WithScheme(scheme).Build()
+	fakeClient.Create(context.TODO(), msr)
+	fakeClient.Create(context.TODO(), device)
+	fakeClient.Create(context.TODO(), manager)
+
+	fs := utils.NewInMemoryFS()
+	AddSpecFilesToFilesystem(t, fs)
+
+	apiClient := kubeapi.NewFakeAPIClient()
+
+	reconciler := ManagerScheduleRequestReconciler{
+		Client:    fakeClient,
+		Scheme:    scheme,
+		FS:        fs,
+		APIClient: apiClient,
+	}
+
+	reconciler.Reconcile(context.TODO(), controllerruntime.Request{NamespacedName: types.NamespacedName{
+		Name: "test-schedule-request",
+	}})
+}
+
+func AddSpecFilesToFilesystem(t *testing.T, fs *utils.InMemoryFS) {
+	t.Helper()
+	if err := fs.AddFileFromHostPath(string(kubeserial.ManagerCMSpecPath)); err != nil {
+		t.Fatalf("Failed to load test asset: %v", err)
+	}
+	if err := fs.AddFileFromHostPath(string(kubeserial.ManagerDeploySpecPath)); err != nil {
+		t.Fatalf("Failed to load test asset: %v", err)
+	}
+	if err := fs.AddFileFromHostPath(string(kubeserial.ManagerSvcSpecPath)); err != nil {
+		t.Fatalf("Failed to load test asset: %v", err)
+	}
 }
